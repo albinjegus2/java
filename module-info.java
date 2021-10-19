34963d1fbfec9ded13a8a1e8b8529bb8e54c7cb1
ALBIN JEGUS <89255221+albinjegus2@users.noreply.github.com>
2021-10-19 17:42:57 +0530

    Update module-info.java

--- module-info.java
+++ module-info.java
@@ -1,7 +1,7 @@
 Public class Fibonaccinumbers {
 	Public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int elementPosition=1,count=10, firstFibonacciNum=0,nextFibonacciNum=1;
+int elementPosition=1,count=20, firstFibonacciNum=0,nextFibonacciNum=1;
 System.out.print(firstFibonacciNum+""+nextFibonacciNum);
 for(elementPosition=2;elementPosition<count;++elementPosition)
 {
